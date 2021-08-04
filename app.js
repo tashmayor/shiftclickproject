1d8db95b5dbf9ef662ef1fc0ac17fc5312d87b8b
tashmayor <mayortash07@gmail.com>
2021-08-04 17:52:46 +0100

    First Commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+var prev = document.getElementById('previous');
+var next = document.getElementById('next');
 
+var arr = ["NBCPoster.jpg", "HalloweenPoster.jpg", "ITPoster.jpg"];
+
+var i = 0;
+
+//next button
+next.addEventListener('click', function(){
+    i++;
+    if (i > arr.length - 1) {
+      i = 0;
+    }
+    document.getElementById('image').src = arr[i];
+})
+
+// prev button
+prev.addEventListener('click', function(){
+    i--;
+    if (i < 0) {
+      i = arr.length - 1;
+    }
+    document.getElementById('image').src = arr[i];
+})
